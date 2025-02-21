af160b6995de4c200eae5ee29a4fcc86ca404618
MahakGupta03 <98971021+MahakGupta03@users.noreply.github.com>
2025-02-21 15:56:32 +0530

    changed

--- src/main/java/com/flatmateFightResolver/flatmateFightResolver/FlatmateFightResolverApplication.java
+++ src/main/java/com/flatmateFightResolver/flatmateFightResolver/FlatmateFightResolverApplication.java
@@ -2,10 +2,8 @@ package com.flatmateFightResolver.flatmateFightResolver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-//@SpringBootApplication
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class FlatmateFightResolverApplication {
 
 	public static void main(String[] args) {
